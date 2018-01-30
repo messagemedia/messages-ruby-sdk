b5a34a82f17c525c6bb33e13b584fe75e7f5092e
James Dale <git@jamesdale.com.au>
2018-01-30 15:00:18 +1100

    Update test_replies_controller test

--- test/controllers/test_replies_controller.rb
+++ test/controllers/test_replies_controller.rb
@@ -56,7 +56,7 @@ class RepliesControllerTests < ControllerTestBase
       # Perform the API call through the SDK function
       self.class.controller.create_confirm_replies_as_received(body, 'INVALID ACCOUNT')
     rescue APIException => api_exception
-      assert_equal('HTTP Response Not OK. {"message":"Invalid account \'INVALID ACCOUNT\' in header Account"}' + "\n",
+      assert_equal('HTTP Response Not OK. {"message":"Invalid authentication credentials"}' + "\n",
                    api_exception.message,'Make sure we''ve got our error message')
 
       # Test response code
