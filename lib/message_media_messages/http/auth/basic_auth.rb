e7307065465d3d0cc6cb3e9cc365a64ef98378ff
James Dale <git@jamesdale.com.au>
2018-02-02 11:09:47 +1100

    Add rest api key length check

--- lib/message_media_messages/http/auth/basic_auth.rb
+++ lib/message_media_messages/http/auth/basic_auth.rb
@@ -12,6 +12,9 @@ module MessageMediaMessages
     def self.apply(http_request, url=nil, body=nil)
       username = Configuration.basic_auth_user_name
       password = Configuration.basic_auth_password
+      if(username.length != 20 || password.length != 30)
+        puts("~~~~ It appears as though your REST API Keys are invalid. Please check and make sure they are correct. (Invalid Length) ~~~~~")
+      end
       value = Base64.strict_encode64("#{username}:#{password}")
       header_value = "Basic #{value}"
       http_request.headers['Authorization'] = header_value
