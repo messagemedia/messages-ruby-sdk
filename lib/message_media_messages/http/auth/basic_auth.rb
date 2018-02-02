4a58e1b213591f11055974331984541329293a02
James Dale <git@jamesdale.com.au>
2018-02-02 16:57:11 +1100

    Check if username/pass is set correctly

--- lib/message_media_messages/http/auth/basic_auth.rb
+++ lib/message_media_messages/http/auth/basic_auth.rb
@@ -12,8 +12,10 @@ module MessageMediaMessages
     def self.apply(http_request, url=nil, body=nil)
       username = Configuration.basic_auth_user_name
       password = Configuration.basic_auth_password
-      if(username.length != 20 || password.length != 30)
-        puts("~~~~ It appears as though your REST API Keys are invalid. Please check and make sure they are correct. (Invalid Length) ~~~~~")
+      if(defined?(username.length) && defined?(password.length))
+        if(username.length != 20 || password.length != 30)
+          puts("~~~~ It appears as though your REST API Keys are invalid. Please check and make sure they are correct. (Invalid Length) ~~~~~")
+        end
       end
       value = Base64.strict_encode64("#{username}:#{password}")
       header_value = "Basic #{value}"
